5c941ff15457fa14e294f784416d79e792207252
James Smith <james@Jamess-MacBook-Air.local>
2019-02-09 21:13:10 -0700

    Updates

---
+++ precache-manifest.374f7ca47511ff75c094585fb0dbe401.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eb0db516d4d438c54450",
+    "url": "/static/js/main.eb0db516.chunk.js"
+  },
+  {
+    "revision": "22e4e537a9ecf7eed8bc",
+    "url": "/static/js/1.22e4e537.chunk.js"
+  },
+  {
+    "revision": "eb0db516d4d438c54450",
+    "url": "/static/css/main.23037e04.chunk.css"
+  },
+  {
+    "revision": "4d58ed3e16929f2f57fda24152fe2f69",
+    "url": "/index.html"
+  }
+];
